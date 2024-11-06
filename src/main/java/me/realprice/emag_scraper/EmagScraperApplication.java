aea66f8756fef354848c58f303491dba6343dd85
Robert <micu.robert24@gmail.com>
2024-11-06 22:18:19 +0200

    Initial commit

---
+++ src/main/java/me/realprice/emag_scraper/EmagScraperApplication.java
@@ -0,0 +1,13 @@
+package me.realprice.emag_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmagScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmagScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/realprice/emag_scraper/EmagScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package me.realprice.emag_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmagScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
